d3b4badb89cc04bda63c53018c8d3315de75cc43
Naiko <89906826+Naeko-sauce@users.noreply.github.com>
2024-05-11 13:16:02 +0800

    甜甜圈

--- components/ThreeJSCubeDemo.vue
+++ components/ThreeJSCubeDemo.vue
@@ -6,8 +6,8 @@ import * as THREE from "three";
 // 目标：了解three.js最基本的内容, 哥哥不会连这个都不会吧，真是杂~鱼❤️呢~~~
 
 
-const width = 800; // 方块宽度
-const height = 800; // 方块高度
+const width = window.innerWidth; // 方块宽度
+const height = window.innerHeight; // 方块高度
 let angle = 0; // 添加一个角度变量来控制圆周运动
 
 onMounted(() => {
