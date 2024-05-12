c6f357dc5f9abe602a463780aefe2c997992fdfa
Naiko <89906826+Naeko-sauce@users.noreply.github.com>
2024-05-12 08:12:50 +0800

    甜甜圈改

--- components/custom/Threejs.vue
+++ components/custom/Threejs.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
 import { onMounted } from "vue";
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+// import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 onMounted(() => {
 
 const scene = new THREE.Scene()
@@ -21,24 +21,24 @@ cont.appendChild(renderer.domElement)
 
 // 创建图形
 const geometry = new THREE.TorusGeometry(10,3,16,100)
-const material = new THREE.MeshBasicMaterial({color:0xFF6347})
+const material = new THREE.MeshBasicMaterial({color:0xFF6347,wireframe:true})
 const torus = new THREE.Mesh(geometry,material)
 scene.add(torus)
 // const cl = new OrbitControls(camera,renderer.domElement)
 //创建光源
 const pointLight = new THREE.PointLight(0xffffff, 1);
-pointLight.position.set(5, 5, 5);
+pointLight.position.set(20, 20, 20);
 pointLight.castShadow = true;
-pointLight.distance = 1000;
+pointLight.distance = 10;
 pointLight.decay = 2;
 
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+const ambientLight = new THREE.AmbientLight(0xffffff, 1);
 scene.add(pointLight, ambientLight);
 
-const gr = new THREE.GridHelper(200, 50);
-scene.add(gr);
+// const gr = new THREE.GridHelper(200, 50);
+// scene.add(gr);
 
-const con  = new OrbitControls(camera,renderer.domElement)
+// const con  = new OrbitControls(camera,renderer.domElement)
 function addStar() {
   const geometry = new THREE.SphereGeometry(0.25, 24, 24);
   const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
@@ -53,15 +53,28 @@ function addStar() {
 }
 
 Array(200).fill().forEach(addStar);
+// 芝士月球
+const moontex = new THREE.TextureLoader().load('/og/moon.jpg')
+const nor = new THREE.TextureLoader().load('/og/normal.jpg')
+const moon = new THREE.Mesh(
+  new THREE.SphereGeometry(5,32,32),
+  new THREE.MeshStandardMaterial({
+    map:moontex,
+    normalMap:nor
+  })
+)
 
+
+
+scene.add(moon)
 const bac = new THREE.TextureLoader().load('/og/space.jpg')
 scene.background = bac
 function am(){
 requestAnimationFrame(am)
-// torus.rotation.x += 0.1;
+moon.rotation.x += 0.01;
 torus.rotation.y += 0.05;
-// torus.rotation.z += 0.1;
-con.update()
+moon.rotation.z += 0.01;
+// con.update()
 
 renderer.render(scene,camera)
 }
