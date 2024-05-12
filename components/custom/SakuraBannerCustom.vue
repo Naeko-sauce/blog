139e1d88835bf15413cfca234c99a7d6b13d19da
Naiko <89906826+Naeko-sauce@users.noreply.github.com>
2024-05-12 09:53:31 +0800

    甜甜圈

---
+++ components/custom/SakuraBannerCustom.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { TresCanvas } from '@tresjs/core';
+</script>
+
+<template>
+  <div class="e">
+  <TresCanvas clear-color="#82DBC5" >
+    <TresPerspectiveCamera
+      :position="[3, 3, 3]"
+      :fov="45"
+      :look-at="[0, 0, 0]"
+    />
+    <TresMesh>
+      <TresTorusGeometry :args="[1, 0.5, 16, 32]" />
+      <TresMeshBasicMaterial color="orange" />
+    </TresMesh>
+    <TresAmbientLight :intensity="1" />
+  </TresCanvas>
+</div>
+</template>
+<style scoped>
+.e{
+  width: 100%;
+  height: 1000px;
+}
+</style>
