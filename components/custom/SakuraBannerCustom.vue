d01bc351090ebfd1e283c7ed17a1773cacee6213
Naiko <89906826+Naeko-sauce@users.noreply.github.com>
2024-05-12 11:31:10 +0800

    甜甜圈主屏幕

--- components/custom/SakuraBannerCustom.vue
+++ components/custom/SakuraBannerCustom.vue
@@ -1,15 +1,13 @@
 <script setup lang="ts">
 import { TresCanvas } from '@tresjs/core';
+import {OrbitControls} from '@tresjs/cientos'
 </script>
 
 <template>
   <div class="e">
   <TresCanvas clear-color="#82DBC5" >
-    <TresPerspectiveCamera
-      :position="[3, 3, 3]"
-      :fov="45"
-      :look-at="[0, 0, 0]"
-    />
+    <TresPerspectiveCamera/>
+    <OrbitControls />
     <TresMesh>
       <TresTorusGeometry :args="[1, 0.5, 16, 32]" />
       <TresMeshBasicMaterial color="orange" />
