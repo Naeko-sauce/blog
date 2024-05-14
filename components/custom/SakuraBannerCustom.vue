0e9490d7ecb9b9998ccb2517d5e5f8931dfe649c
Naiko <89906826+Naeko-sauce@users.noreply.github.com>
2024-05-14 18:41:42 +0800

    修改车模型

--- components/custom/SakuraBannerCustom.vue
+++ components/custom/SakuraBannerCustom.vue
@@ -6,13 +6,13 @@ import { OrbitControls } from '@tresjs/cientos'
 <template>
   <div class="e">
   <TresCanvas shadows clear-color='rgb(0,0,0)' >
-    <TresPerspectiveCamera :position="[1,4,7]" :fov='60'/>
+    <TresPerspectiveCamera :position="[3,4,5]" :fov='70'/>
 
     <OrbitControls :target="[0,0,0,0]" :max-polar-angle="1.45" :enable-zoom="false"/>
     <Suspense>
       <!-- <Car /> -->
-      <!-- <Cars /> -->
-      <Angel />
+      <Cars />
+      <!-- <Angel /> -->
     </Suspense>
     <Suspense>
     <Ground />
@@ -23,7 +23,7 @@ import { OrbitControls } from '@tresjs/cientos'
 
 
     <TresSpotLight
-
+    :color='[2,0.25,0.7]'
     :intensity="250"
     :angle="0.6"
     :penumbra="0.5"
@@ -31,7 +31,7 @@ import { OrbitControls } from '@tresjs/cientos'
     :position='[5,5,0]'
     />
     <TresSpotLight
-
+    :color='[0.1,0.5,0.7]'
     :intensity="400"
     :angle="0.6"
     :penumbra="0.5"
