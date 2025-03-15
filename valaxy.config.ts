fd3dba136d08ae346bf354e1ae62c97f6780b22d
naiko <3443471283@qq.com>
2025-03-15 23:49:56 +0800

    chore:梦的开始

---
+++ valaxy.config.ts
@@ -0,0 +1,34 @@
+import type { UserThemeConfig } from 'valaxy-theme-sakura'
+import { defineValaxyConfig } from 'valaxy'
+
+// add icons what you will need
+const safelist = [
+  'i-ri-home-line',
+]
+
+/**
+ * User Config
+ */
+export default defineValaxyConfig<UserThemeConfig>({
+  // site config see site.config.ts
+
+  theme: 'sakura',
+
+  themeConfig: {
+    
+    ui: {
+      primary: '#ff4e6a',
+    },
+
+    hero: {
+      title: 'Hello, naiko',
+      motto: 'blog',
+      urls: [
+        'https://wrxinyue-images.s3.bitiful.net/wallpaper/Genshin Impact - Yae Miko (4) Cybust PC.mp4',
+        'https://wrxinyue-images.s3.bitiful.net/pc-wallpaper/wallhaven-yxwy7k.jpg'
+      ],
+      style: 'filter-dim',
+    },
+  
+  },
+})
